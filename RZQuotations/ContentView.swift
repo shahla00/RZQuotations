d9d060a99c01f03578330b3e8ef42c688a22aa03
Shahla Almasri Hafez <shahla@rzsolutionsinc.com>
2019-10-19 13:35:44 -0400

    Project shell

---
+++ RZQuotations/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RZQuotations
+//
+//  Created by Shahla Almasri Hafez on 10/19/19.
+//  Copyright © 2019 Shahla Almasri Hafez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
